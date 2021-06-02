d642f571976f9aa11e201b8d36d1a8e2ccc3f8a5
PioKl <kosiu342@gmail.com>
2021-06-02 21:24:23 +0200

    Add getStaticProps function to fetch drinks from database and now it is possible to displaying all the drinks from the database

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
+import { API_URL } from "@/config/index";
+import DrinkCard from "@/components/DrinkCard";
 
 
-export default function Home() {
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/drinks`);
+  const drinks = await res.json()
+
+  return {
+    props: { drinks },
+    revalidate: 1,
+  }
+}
+
+export default function Home({ drinks }) {
+
   return (
     <div>
       <h1>Main Page</h1>
+      {drinks.length === 0 && <h1>Nie ma drinków</h1>}
+      {drinks.map(drink => (
+        <DrinkCard key={drink.id} drink={drink} />
+      ))}
     </div>
   )
 }
+
+
+
+/* export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/drinks`);
+  const drinks = await res.json()
+
+  return {
+    props: { drinks },
+  }
+}
+ */
