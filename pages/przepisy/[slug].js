70377f48fd9574c043abccec2a377c15d2e30549
PioKl <kosiu342@gmail.com>
2021-06-02 21:28:16 +0200

    Appropriate slug paths have been added with getStaticPaths function and the details of chosed drink can now be displayed (getStaticProps function)

--- pages/przepisy/[slug].js
+++ pages/przepisy/[slug].js
@@ -1,7 +1,53 @@
-export default function DrinkDetails() {
+import {API_URL} from "@/config/index";
+
+
+export async function getStaticPaths(){
+    const res = await fetch(`${API_URL}/drinks`)
+    const drinks = await res.json()
+
+    const paths = drinks.map(drink => ({
+        params: {slug: drink.slug}
+    }));
+
+    return {
+        paths,
+        fallback: true,
+    }
+}
+
+//te params ponizej pochodza, z paramsów z getStaticPaths
+export async function getStaticProps({params: {slug}}) {
+
+    const res = await fetch (`${API_URL}/drinks?slug=${slug}`);
+    const drinks = await res.json();
+    return {
+        props:{
+            drink: drinks[0],
+            revalidate: 1,
+        }
+    }
+} 
+
+
+
+export default function DrinkDetails({drink}) {
     return (
         <div>
-            <p>Szczegóły</p>
+            <h1>{drink.name}</h1>
+            <img src={drink.image.formats.thumbnail.url} width="200px" height="200px" alt="" />
         </div>
     )
 }
+
+
+
+/* export async function getServerSideProps({query: {slug}}) {
+
+    const res = await fetch (`${API_URL}/api/przepisy/${slug}`)
+    const drinks = await res.json
+    return {
+        props:{
+            drink: drinks[0]
+        }
+    }
+} */
