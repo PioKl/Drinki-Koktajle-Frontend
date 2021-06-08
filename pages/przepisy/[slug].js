e9bc3f5101d4a1a8d1e15ea8866d315be4b197f5
PioKl <kosiu342@gmail.com>
2021-06-08 23:31:13 +0200

    Add measures array to display measures list  and return to main page button

--- pages/przepisy/[slug].js
+++ pages/przepisy/[slug].js
@@ -33,14 +33,17 @@ export async function getStaticProps({ params: { slug } }) {
 
 
 export default function DrinkDetails({ drink }) {
-    console.log(drink)
-    const { ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6 } = drink;
-    const ingredientList = [ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6,];
-    console.log(ingredientList)
 
-    //USUWANIE
     const router = useRouter();
 
+    console.log(drink)
+    const { ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, measure1, measure2, measure3, measure4, measure5, measure6 } = drink;
+    const ingredientList = [ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6,];
+    console.log(ingredientList)
+    const measureList = [measure1, measure2, measure3, measure4, measure5, measure6];
+
+
+    //USUWANIE
     const handleDeleteDrink = async (e) => {
         if (confirm('Na pewno')) {
             const res = await fetch(`${API_URL}/drinks/${drink.id}`, {
@@ -58,6 +61,7 @@ export default function DrinkDetails({ drink }) {
     }
     return (
         <div>
+            <Link href="/"><a><button>Strona Główna</button></a></Link>
             <button onClick={handleDeleteDrink}>Delete</button>
             <Link href={`/przepisy/edytuj/${drink.id}`}><a><button>Edit</button></a></Link>
             <h1>{drink.name}</h1>
@@ -67,6 +71,9 @@ export default function DrinkDetails({ drink }) {
 
             {ingredientList.map((ingredient, id) => ingredient ?
                 <li key={id}>{ingredient}</li> : null
+            )}
+            {measureList.map((measure, id) => measure ?
+                <li key={id}>{measure}</li> : null
             )}
             {drink.image && <img src={drink.image.formats.thumbnail.url} width="200px" height="200px" alt="" />}
         </div>
