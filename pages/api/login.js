5436cb8a5715be3581ea726f51cf0e26ab41e6b8
PioKl <kosiu342@gmail.com>
2021-06-26 17:04:33 +0200

    Add login to check if data from AuthContext are consistent with the data from strapi database, then sending this data to AuthContext or sending an error message

---
+++ pages/api/login.js
@@ -0,0 +1,41 @@
+import {API_URL} from '@/config/index';
+
+export default async (request, response) => {
+    //jeśli żądana metoda to POST wtedy przejdź dalej
+    if(request.method === 'POST') {
+        //identifier i password pochodzą z AuthContext, który "przesyła dane" do api/login
+        const {identifier, password} = request.body;
+
+        //fetch tokena ze strapi, czyli zalogowanie użytkownika za pomocą strapi
+        //strapi request
+        const strapiResponse = await fetch(`${API_URL}/auth/local`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                identifier,
+                password
+            })
+        })
+        //po przesłaniu danych do strapi, jest sprawdzane, czy login i hasło jest poprawne z danymi w bazie danych strapi
+
+        const data = await strapiResponse.json()
+
+        //Jeśli dane są prawidłowe to ustaw te dane jako user i te dane, będą odebrane w AuthContext
+        if(strapiResponse.ok) {
+            response.status(200).json({user: data.user});
+        } //Jeśli zły login, to przekaż błędną wiadomość (np. zły login, hasło)
+        else {
+            response.status(data.statusCode).json({message: data.message[0].messages[0].message})
+        }
+
+        
+    } 
+    //Jeśli jest to inna metoda niż POST
+    else {
+        response.setHeader('Allow', ['POST'])
+        response.status(405).json({message: `Method ${request.method} not allowed`});
+    }
+}
