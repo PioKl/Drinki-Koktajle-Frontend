a25c1fc308c078ea78e8fa8974eec8a8b1e0ba1b
PioKl <kosiu342@gmail.com>
2021-06-26 14:27:43 +0200

    Add AuthContext into Header to show and hide options for logged and logged off user

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,33 @@
 import Link from 'next/link';
+import { useContext } from 'react';
+import AuthContext from '@/context/AuthContext';
 
 export default function Header() {
+
+    const { user, logout } = useContext(AuthContext)
+
     return (
         <div>
             <Link href="/">
                 <a>Strona Główna </a>
             </Link>
-            <Link href="/przepisy/dodaj-drinka">
-                <a>Dodaj Drinka </a>
-            </Link>
-            <Link href="/konto/zaloguj" >
-                <a>Zaloguj </a>
-            </Link>
+            {user ?
+                //Jeśli użytkownik jest zalogowany
+                <>
+                    <Link href="/przepisy/dodaj-drinka">
+                        <a>Dodaj Drinka </a>
+                    </Link>
+                    <Link href="/konto/moje-drinki">
+                        <a>Moje Drinki </a>
+                    </Link>
+                    <button onClick={() => logout()}>Wyloguj</button>
+                </>
+                :
+                //Jeśli użytkownik nie jest zalogowany
+                <Link href="/konto/zaloguj" >
+                    <a>Zaloguj </a>
+                </Link>
+            }
         </div>
     )
 }
