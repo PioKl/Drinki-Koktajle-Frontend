1e5a475ea6732b01351a6c3c77d9b82e2d95cc15
PioKl <kosiu342@gmail.com>
2021-06-26 14:29:23 +0200

    Add AuthContext into zaloguj to send some values into login in AuthContext from zaloguj

--- pages/konto/zaloguj.js
+++ pages/konto/zaloguj.js
@@ -2,13 +2,17 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { useState, useEffect, useContext } from "react";
 import Link from 'next/link';
+import AuthContext from "@/context/AuthContext";
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const {login, error} = useContext(AuthContext);
     
     const handleSubmit = (e) => {
         e.preventDefault();
+        login({email, password})
     }
 
     return (
