7190e6ad2775ec2aae5413fbb9b18e39b2750f2b
PioKl <kosiu342@gmail.com>
2021-08-29 21:24:32 +0200

    Add Google Fonts

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,24 @@
 import Head from 'next/head'
 import Header from './Header';
 
-export default function Layout({title, keywords, description, children}) {
+export default function Layout({ title, keywords, description, children }) {
     return (
         <div>
             <Head>
                 <title>{title}</title>
-                <meta name='description' content={description}/>
-                <meta name='keywords' content={keywords}/>
+                <meta name='description' content={description} />
+                <meta name='keywords' content={keywords} />
+                
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Rasa:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet" />
             </Head>
 
-            <Header />
+            {/* <Header /> */}
             <div className="page-content">
                 {children}
             </div>
-            
+
         </div>
     )
 }
