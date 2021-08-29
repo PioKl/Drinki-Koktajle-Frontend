5663acdaee4651c1172389a4d3bb056c37cbd6d4
PioKl <kosiu342@gmail.com>
2021-08-29 21:23:49 +0200

    Add svgr webpack

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 module.exports = {
     images: {
         domains: ['res.cloudinary.com'],
-    }
+    },
+    webpack(config) {
+        config.module.rules.push({
+          test: /\.svg$/,
+          issuer: {
+            test: /\.(js|ts)x?$/,
+           // for webpack 5 use
+           // { and: [/\.(js|ts)x?$/] }
+          },
+          
+          use: ['@svgr/webpack'],
+        });
+    
+        return config;
+      },
 }
